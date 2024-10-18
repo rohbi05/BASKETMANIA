dcb6f66be39c4d8cad27bcfcdec443dec167dd73
shantalrohbi <shantal.rohbi@student.moringaschool.com>
2024-10-18 14:04:57 +0300

    crated tthe teams model

--- BASKETMANIA/BACKEND/models.py
+++ BASKETMANIA/BACKEND/models.py
@@ -14,3 +14,18 @@ class User(db.Model):
             'email': self.email,
             'teams': [team.to_dict() for team in self.teams]
         }
+    # Team model (One-to-Many with Player, Many-to-One with User)
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    players = db.relationship('TeamPlayer', backref='team', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id,
+            'players': [team_player.to_dict() for team_player in self.players]
+        }
