e58c2e299e43971661ccaf86a757ed18fadf785f
shantalrohbi <shantal.rohbi@student.moringaschool.com>
2024-10-18 14:12:52 +0300

    created the player  models

--- BASKETMANIA/BACKEND/models.py
+++ BASKETMANIA/BACKEND/models.py
@@ -29,3 +29,26 @@ class Team(db.Model):
             'user_id': self.user_id,
             'players': [team_player.to_dict() for team_player in self.players]
         }
+    # Player model (Many-to-Many with Team through TeamPlayer)
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    position = db.Column(db.String(80), nullable=False)
+    height = db.Column(db.Float)  # Assuming height is a float
+    weight = db.Column(db.Float)  # Assuming weight is a float
+    birthdate = db.Column(db.Date)  # Assuming birthdate is a date
+    image_url = db.Column(db.String(255))
+
+    teams = db.relationship('TeamPlayer', backref='player', lazy=True)
+
+def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'position': self.position,
+            'height': self.height,  # Include height in to_dict
+            'weight': self.weight,   # Include weight in to_dict
+            'teams': [team_player.to_dict() for team_player in self.teams]
+        }
