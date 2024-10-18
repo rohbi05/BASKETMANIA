59ff02cd992020a30c7e38151e27065b75b687ba
Adrian Kahura <kahuraadrian@gmail.com>
2024-10-18 13:42:32 +0300

    user model

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -12,3 +12,10 @@ db_config = {
     "database": "your_database"
 }
 
+# User model
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
