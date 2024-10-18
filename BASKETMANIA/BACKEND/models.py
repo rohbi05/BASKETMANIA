8175d0252d550f10090dbead7c74e637fb9e0976
shantalrohbi <shantal.rohbi@student.moringaschool.com>
2024-10-18 13:53:46 +0300

    created the user model

---
+++ BASKETMANIA/BACKEND/models.py
@@ -0,0 +1,16 @@
+
+# User model (One-to-Many with Team)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    teams = db.relationship('Team', backref='user', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'teams': [team.to_dict() for team in self.teams]
+        }
