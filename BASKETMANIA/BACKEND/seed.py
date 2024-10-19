2d117b5349ac89782684d3544f111faff8e3f4b7
Eugene Mathenge <eugene@Eugene.eugenemathenge4@gmail.com>
2024-10-19 05:49:16 -0400

    added player information to the seed.py file

--- BASKETMANIA/BACKEND/seed.py
+++ BASKETMANIA/BACKEND/seed.py
@@ -1,2 +1,56 @@
 from models import User, Team, Player, TeamPlayer
 from datetime import datetime
+
+player_data = [
+    {
+        "player_id": "1",
+        "name": "LeBron James",
+        "team": "Cleveland Cavaliers",
+        "position": "Forward",
+        "height": "6'9\"",
+        "weight": "250 lbs",
+        "birthdate": "1984-12-30",
+        "image_url": "https://i.pinimg.com/564x/6a/ae/f7/6aaef74808fdfbe4b25c41699fba6d81.jpg"
+    },
+    {
+        "player_id": "2",
+        "name": "Kevin Durant",
+        "team": "Los Angeles Lakers",
+        "position": "Forward",
+        "height": "6'10\"",
+        "weight": "240 lbs",
+        "birthdate": "1988-09-29",
+        "image_url": "https://i.pinimg.com/236x/23/18/93/2318930549a4f8240f01722d860e178b.jpg"
+    },
+    {
+        "player_id": "3",
+        "name": "Giannis Antetokounmpo",
+        "team": "Milwaukee Bucks",
+        "position": "Forward",
+        "height": "6'11\"",
+        "weight": "242 lbs",
+        "birthdate": "1994-12-06",
+        "image_url": "https://i.pinimg.com/564x/a0/d6/f0/a0d6f04ddb9f5a193eefb408a4aa253f.jpg"
+    },
+    {
+        "player_id": "4",
+        "name": "Stephen Curry",
+        "team": "Los Angeles Clippers",
+        "position": "Guard",
+        "height": "6'2\"",
+        "weight": "185 lbs",
+        "birthdate": "1988-03-14",
+        "image_url": "https://i.pinimg.com/236x/99/76/01/997601ff2e50374e90347a5aba290316.jpg"
+    },
+    {
+        "player_id": "5",
+        "name": "Luka Dončić",
+        "team": "Dallas Mavericks",
+        "position": "Guard/Forward",
+        "height": "6'7\"",
+        "weight": "230 lbs",
+        "birthdate": "1999-02-28",
+        "image_url": "https://i.pinimg.com/236x/ae/0e/22/ae0e2264f9f72841c52a7feb9a0a99a1.jpg"
+    }
+    
+]
