074483c70e5401a7ca41dfca048d1b9212d5a655
Eugene Mathenge <eugene@Eugene.eugenemathenge4@gmail.com>
2024-10-19 05:50:47 -0400

    added player information to the seed.py file

--- BASKETMANIA/BACKEND/seed.py
+++ BASKETMANIA/BACKEND/seed.py
@@ -51,6 +51,56 @@ player_data = [
         "weight": "230 lbs",
         "birthdate": "1999-02-28",
         "image_url": "https://i.pinimg.com/236x/ae/0e/22/ae0e2264f9f72841c52a7feb9a0a99a1.jpg"
+    },
+    {
+        "player_id": "6",
+        "name": "Joel Embiid",
+        "team": "Philadelphia 76ers",
+        "position": "Center",
+        "height": "7'0\"",
+        "weight": "280 lbs",
+        "birthdate": "1994-03-16",
+        "image_url": "https://i.pinimg.com/236x/69/8b/27/698b274894766b3e3c55786b53c861b9.jpg"
+    },
+    {
+        "player_id": "7",
+        "name": "Zion Williamson",
+        "team": "New Orleans Pelicans",
+        "position": "Forward",
+        "height": "6'6\"",
+        "weight": "284 lbs",
+        "birthdate": "2000-07-06",
+        "image_url": "https://i.pinimg.com/236x/cf/d1/47/cfd1476b8f49d4ecf3c742d77b668b80.jpg"
+    },
+    {
+        "player_id": "8",
+        "name": "Myles Turner",
+        "team": "Golden State Warriors",
+        "position": "Center",
+        "height": "6'10\"",
+        "weight": "250 lbs",
+        "birthdate": "1996-03-24",
+        "image_url": "https://www.basketballnetwork.net/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1290/MTkyNzQ0MDkxMTkxMDkyMjUz/myles-turner.jpg"
+    },
+    {
+        "player_id": "9",
+        "name": "Pascal Siakam",
+        "team": "Phoenix Suns",
+        "position": "Forward",
+        "height": "6'9\"",
+        "weight": "230 lbs",
+        "birthdate": "1994-04-02",
+        "image_url": "https://basketnews.com/image-415326-crop700x700.jpg"
+    },
+    {
+        "player_id": "10",
+        "name": "Darius Garland",
+        "team": "Boston Celtics",
+        "position": "Guard",
+        "height": "6'1\"",
+        "weight": "192 lbs",
+        "birthdate": "2000-01-26",
+        "image_url": "https://i.pinimg.com/236x/5b/b7/41/5bb7418244b5323c350dc12768e0bb66.jpg"
     }
     
 ]
